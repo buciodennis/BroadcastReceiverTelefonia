d0b764676851ad66538d1edd2913a11ab23f0f23
buciodennis <s18120175@alumnos.itsur.edu.mx>
2022-02-10 15:49:30 -0600

    ShredPreferences

--- app/src/main/java/com/example/broadcastreceivertelefonia/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceivertelefonia/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.broadcastreceivertelefonia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.broadcastreceivertelefonia.receivers.MiBroadcastReceiver;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText txtNumero;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtNumero= findViewById(R.id.txtNumero);
+
+        findViewById(R.id.btnAceptar).setOnClickListener(v->{
+            SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+            SharedPreferences.Editor myEdit = sharedPreferences.edit();
+            myEdit.putString("numero", txtNumero.getText().toString());
+            myEdit.commit();
+        });
     }
+
+
+
 }
--- app/src/main/java/com/example/broadcastreceivertelefonia/receivers/MiBroadcastReceiver.java
+++ app/src/main/java/com/example/broadcastreceivertelefonia/receivers/MiBroadcastReceiver.java
@@ -1,22 +1,37 @@
 package com.example.broadcastreceivertelefonia.receivers;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
 public class MiBroadcastReceiver extends BroadcastReceiver {
+
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        SharedPreferences sh = context.getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        String numero = sh.getString("numero","");
+
         String action = intent.getAction();
 
         if (action.equals(android.telephony.TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
-            String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-            Log.d("Llamada", "Received call: " + incomingNumber);
-            Toast.makeText(context, "Llamada recibida", Toast.LENGTH_SHORT).show();
+           // Toast.makeText(context, "Llamada recibida", Toast.LENGTH_SHORT).show();
+           Toast.makeText(context, "Numero"+numero,Toast.LENGTH_SHORT );
         }
+
+    }
+
+
+
+    private void enviarSMS() {
 
     }
 }
