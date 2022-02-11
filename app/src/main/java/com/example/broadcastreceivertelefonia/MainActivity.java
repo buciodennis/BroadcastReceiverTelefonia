62344f435f78e85c0e49d1d5acc529cc335ddaa6
buciodennis <s18120175@alumnos.itsur.edu.mx>
2022-02-11 16:01:03 -0600

    Registro de numero en objeto SharedPreferences

--- app/src/main/java/com/example/broadcastreceivertelefonia/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceivertelefonia/MainActivity.java
@@ -7,25 +7,37 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.broadcastreceivertelefonia.receivers.MiBroadcastReceiver;
 
 public class MainActivity extends AppCompatActivity {
     EditText txtNumero;
+    TextView txtDefinido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         txtNumero= findViewById(R.id.txtNumero);
+        txtDefinido=findViewById(R.id.txtDefinido);
+
+        SharedPreferences sh = this.getSharedPreferences("MiSP", MODE_PRIVATE);
+        String numero = sh.getString("numero","");
+        txtDefinido.setText(numero);
 
         findViewById(R.id.btnAceptar).setOnClickListener(v->{
-            SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+
+            String num= txtNumero.getText().toString();
+            SharedPreferences sharedPreferences = this.getSharedPreferences("MiSP",MODE_PRIVATE);
             SharedPreferences.Editor myEdit = sharedPreferences.edit();
-            myEdit.putString("numero", txtNumero.getText().toString());
-            myEdit.commit();
+
+            myEdit.putString("numero",num );
+            myEdit.apply();
+            txtDefinido.setText(num);
         });
     }
 
