71f7224b1933706fdbc1120298963b68384faceb
AlexiaAM <alexia77755@gmail.com>
2022-02-11 18:05:50 -0600

    send message

--- app/src/main/java/com/example/broadcastreceivertelefonia/receivers/MiBroadcastReceiver.java
+++ app/src/main/java/com/example/broadcastreceivertelefonia/receivers/MiBroadcastReceiver.java
@@ -8,30 +8,42 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
 public class MiBroadcastReceiver extends BroadcastReceiver {
 
-
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        SharedPreferences sh = context.getSharedPreferences("MySharedPref", MODE_PRIVATE);
-        String numero = sh.getString("numero","");
-
+        SharedPreferences sh = context.getSharedPreferences("MiSP", MODE_PRIVATE);
+        String numero = sh.getString("numero","0");
         String action = intent.getAction();
 
-        if (action.equals(android.telephony.TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
-           // Toast.makeText(context, "Llamada recibida", Toast.LENGTH_SHORT).show();
-           Toast.makeText(context, "Numero"+numero,Toast.LENGTH_SHORT );
+        if (action.equals(android.telephony.TelephonyManager.ACTION_PHONE_STATE_CHANGED)&& intent
+                .getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+
+            if(intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)!=null){
+                String numeroEntrante= intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+
+                if(numeroEntrante.equals(numero)){
+                    Log.d("Numeros:","Coinciden");
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage("4451097898",
+                            null,
+                            "No puedo contestar ahora",
+                            null, null);
+                }
+            }
+
+
+
+
         }
 
     }
 
 
 
-    private void enviarSMS() {
 
-    }
 }
